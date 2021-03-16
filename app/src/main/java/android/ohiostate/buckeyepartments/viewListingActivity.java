b81b8baabf740149843037cc13d4b58f63325f47
icereg1992 <willwill.sullivan@gmail.com>
2021-03-15 20:42:50 -0400

    Rebase onto master

--- app/src/main/java/android/ohiostate/buckeyepartments/viewListingActivity.java
+++ app/src/main/java/android/ohiostate/buckeyepartments/viewListingActivity.java
@@ -23,6 +23,9 @@ import java.util.Map;
 
 import java.util.Objects;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class viewListingActivity extends AppCompatActivity {
 
     private DatabaseReference ref;
